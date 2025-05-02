2f403740fbfdcda7e74f86bb1d11fc5bd47e2f6f
Sunwoo Jung <104188728+willjsw@users.noreply.github.com>
2025-05-02 22:23:23 +0900

    KW-260/refactor: AuthHeaderFilter -> JWTAuthenticaionFilter 변형 및 SecurityContextHolder 활욯한 인증 로직 추가 (#12)

--- src/main/java/com/doubleo/mobilegateway/infra/config/properties/PropertiesConfig.java
+++ src/main/java/com/doubleo/mobilegateway/infra/config/properties/PropertiesConfig.java
@@ -1,8 +1,8 @@
 package com.doubleo.mobilegateway.infra.config.properties;
 
-import com.doubleo.mobilegateway.infra.config.gateway.GatewayPathProperties;
 import com.doubleo.mobilegateway.infra.config.jwt.JwtProperties;
 import com.doubleo.mobilegateway.infra.config.redis.RedisProperties;
+import com.doubleo.mobilegateway.infra.config.route.GatewayPathProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
