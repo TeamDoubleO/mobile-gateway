a5ee792753be16fbb8713ce90dcd309162ac3392
willjsw <sunwoo1137@gmail.com>
2025-05-02 15:20:03 +0900

    KW-249/refactor: 패키지 변경후 남은 legacy files 삭제

---
+++ src/test/java/com/doubleo/apigateway/MobileGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doubleo.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
