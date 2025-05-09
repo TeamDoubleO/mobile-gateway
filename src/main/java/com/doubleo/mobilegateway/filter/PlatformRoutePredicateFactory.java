ccf917fe432047f8d067376e4479a2095ebb956b
Sunwoo Jung <104188728+willjsw@users.noreply.github.com>
2025-05-09 17:26:18 +0900

    KW-333/feat: platform predicate factory 추가 (#16)

---
+++ src/main/java/com/doubleo/mobilegateway/filter/PlatformRoutePredicateFactory.java
@@ -0,0 +1,59 @@
+package com.doubleo.mobilegateway.filter;
+
+import com.doubleo.mobilegateway.infra.config.jwt.JwtProperties;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.ws.rs.core.HttpHeaders;
+import java.nio.charset.StandardCharsets;
+import java.util.function.Predicate;
+import lombok.Data;
+import org.springframework.cloud.gateway.handler.predicate.AbstractRoutePredicateFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+@Component
+public class PlatformRoutePredicateFactory
+        extends AbstractRoutePredicateFactory<PlatformRoutePredicateFactory.Config> {
+
+    private final JwtProperties jwtProperties;
+
+    public PlatformRoutePredicateFactory(JwtProperties jwtProperties) {
+        super(Config.class);
+        this.jwtProperties = jwtProperties;
+    }
+
+    @Override
+    public Predicate<ServerWebExchange> apply(Config config) {
+        return exchange -> {
+            String authHeader =
+                    exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+            if (authHeader == null || !authHeader.startsWith("Bearer ")) return false;
+
+            try {
+                String token = authHeader.substring(7);
+                Claims claims =
+                        Jwts.parserBuilder()
+                                .setSigningKey(
+                                        Keys.hmacShaKeyFor(
+                                                jwtProperties
+                                                        .accessTokenSecret()
+                                                        .getBytes(StandardCharsets.UTF_8)))
+                                .build()
+                                .parseClaimsJws(token)
+                                .getBody();
+
+                String platform = claims.get("platform", String.class);
+                return config.getPlatform().equalsIgnoreCase(platform);
+
+            } catch (Exception e) {
+                return false;
+            }
+        };
+    }
+
+    @Data
+    public static class Config {
+        private String platform;
+    }
+}
